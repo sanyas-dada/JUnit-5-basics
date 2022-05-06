921682f99c8b57fd697e1d3a3e9d92f0de9ec9a3
sanyas-dada <sanyasdada09@gmail.com>
2022-05-06 22:21:20 +0545

    first commit

--- src/test/java/com/sanyas/junit/MathUtilsTest.java
+++ src/test/java/com/sanyas/junit/MathUtilsTest.java
@@ -9,11 +9,19 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestReporter;
 
 
 //@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
+	
+	TestInfo testInfo;
+	TestReporter testReporter;
 	MathUtils mathUtils;
 	
 	
@@ -23,24 +31,43 @@ class MathUtilsTest {
 	 */
 	
 	@BeforeEach
-	void init() {
-		 mathUtils = new MathUtils();
+	void init(TestInfo testInfo, TestReporter testReporter) {
+		this.testInfo = testInfo;
+		this.testReporter = testReporter;
+		mathUtils = new MathUtils();
+		
+		//System.out.println("Runnig:" +testInfo.getDisplayName()+"with tags" +testInfo.getTags());
+		//testReporter.publishEntry("Running" +testInfo.getDisplayName()+" with tags" +testInfo.getTags());
 	}
 	/*
 	 * @AfterEach void cleanup() { System.out.println("Cleaning up>>>>"); }
 	 */
+	@Nested
+	@Tag("Math")
+	class add{
+		@Test
+		@DisplayName("when adding positive number")
+		void testPositiveNumber() {
+			//System.out.println("This is test running");
+			// Junit hack can be used as a runner!
+			int expected = 2;
+			int actual = mathUtils.add(1, 1);
+			assertEquals(expected,actual, "the add method should add two numbers");
+		}
+		@Test
+		@DisplayName("when adding negative number ")
+		void testNegativeNumber() {
+			//System.out.println("This is test running");
+			// Junit hack can be used as a runner!
+			int expected = -2;
+			int actual = mathUtils.add(-1, -1);
+			assertEquals(expected,actual, "the add method should add two numbers");
+		}
+	}
 	
 	@Test
-	@DisplayName("testing add method")
-	void test() {
-		//System.out.println("This is test running");
-		// Junit hack can be used as a runner!
-		int expected = 2;
-		int actual = mathUtils.add(1, 1);
-		assertEquals(expected,actual, "the add method should add two numbers");
-	}
-	@Test
 	@DisplayName("multiply method")
+	@Tag("Math")
 	void testMultiply() {
 		assertAll(
 				() -> assertEquals(4,mathUtils.mul(2,2)),
@@ -50,6 +77,7 @@ class MathUtilsTest {
 				);
 	}
 	@Test
+	@Tag("Math")
 	void testDivideRules() {
 		boolean isServerUp = false;
 		assumeTrue(isServerUp);
@@ -57,8 +85,9 @@ class MathUtilsTest {
 		assertThrows(ArithmeticException.class,() -> mathUtils.divide(1, 0),"Divide by zero");
 	}
 	
-	@Test
+	@RepeatedTest(2)
 	@DisplayName("area Of the Circle")
+	@Tag("Circle")
 	void testComputeCircleRadius() {
 		assertEquals(Math.PI,mathUtils.computeCircleArea(1),"this is the circle of the area ");
 	}
